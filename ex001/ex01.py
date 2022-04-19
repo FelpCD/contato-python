97548eb827973e7df3a282f54a2c32a680807baf
Felps <felipecsilva391@gmail.com>
2022-04-19 17:32:05 -0300

    Primeiro contato com o a linguagem Python

---
+++ ex001/ex01.py
@@ -0,0 +1,13 @@
+#Alguns dos tipos de dados que podem ser armasenados em um computador: 
+#Números - inteiro 
+#Float - Para falores decimais 
+#Vareaveis boleanos - False or True
+#String - ex: "digite o seu nome "
+
+#Variaveis usada em um problema real! 
+#Valor do salário por hora trabalhada por mês
+
+salario_mes = input('Qual o seu salário mensal? ')
+hora_mes = input("Qantas hora você trabalha por mês? ")
+valor_hora  = int(salario_mes) / int(hora_mes)
+print(valor_hora)
